68bf080cf918015ea8569a74ff9f296305e9f494
Massimiliano Porzio <massimiliano.porzio@gmail.com>
2025-01-02 22:13:50 +0100

    login ?

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,4 +1,4 @@
-import {Request, Response} from 'express';
+import { Request, Response } from 'express';
 import pool from '../models/db';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
@@ -6,46 +6,47 @@ import jwt from 'jsonwebtoken';
 const SALT_ROUNDS = 10;
 const JWT_SECRET = process.env.JWT_SECRET || 'worisecretkey'
 
-export const register = async(req: Request, res: Response) => {
- //1. get username, password, name, email
- //2. query db per fare insert in db
- //3. return message (good or not)
- const {username,email,password } = req.body;
- try {
-    const hashedPassword = await bcrypt.hash(password,SALT_ROUNDS);
-    const result = await pool.query(
-        'INSERT INTO users (username,email,password) VALUES ($1,$2,$3) RETURNING *',
-        [username,email,hashedPassword]
-    );
-    const user = result.rows[0]; //prima riga di risultati (ne devo avere solo una)
-    res.status(201).json({message: 'User registered successfully',user});
- } catch (error) {
-    res.status(500).json({error: 'Failed to register user'});
- }
+export const register = async (req: Request, res: Response) => {
+   //1. get username, password, name, email
+   //2. query db per fare insert in db
+   //3. return message (good or not)
+   const { username, email, password } = req.body;
+   try {
+      const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
+      const result = await pool.query(
+         'INSERT INTO users (username,email,password) VALUES ($1,$2,$3) RETURNING *',
+         [username, email, hashedPassword]
+      );
+      const user = result.rows[0]; //prima riga di risultati (ne devo avere solo una)
+      res.status(201).json({ message: 'User registered successfully', user });
+   } catch (error) {
+      res.status(500).json({ error: 'Failed to register user' });
+   }
 }
 
-export const login = async(req: Request, res: Response) => {
-    //1. get email and password
-    //2. verify if email exists than check password
-    //3. return message if pwd wrong
-    //4. return TOKEN!
-    const {email, password} = req.body;
-    try {
-    
-    const result = await pool.query(
-        'SELECT * FROM users WHERE email = $1 ',
-        [email]
-    );
-    const user = result.rows[0]; //prima riga di risultati (ne devo avere solo una)
-    res.status(201).json({message: 'User registered successfully',user});
-    if(!user) return res.status(404).json({error: 'User not found'});
+export const login = async (req: Request, res: Response): Promise<any> => {
+   console.log("UFFA");
+   //1. get email and password
+   //2. verify if email exists than check password
+   //3. return message if pwd wrong
+   //4. return TOKEN!
+   const { email, password } = req.body;
+   try {
 
-    const isMatch = await bcrypt.compare(password, user.password);
-    if(!isMatch) return res.status(400).json({error: 'Invalid credentials'});
+      const result = await pool.query(
+         'SELECT * FROM users WHERE email = $1 ',
+         [email]
+      );
+      const user = result.rows[0]; //prima riga di risultati (ne devo avere solo una)
+      console.log('ciao');
+      if (!user) return res.status(404).json({ error: 'User not found' });
 
-    const token = jwt.sign({id: user.id}, JWT_SECRET, {expiresIn: '10h'});
-    res.json()
- } catch (error) {
-    res.status(500).json({error: 'Failed to register user'});
- }
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) return res.status(400).json({ error: 'Invalid credentials' });
+
+      const token = jwt.sign({ id: user.id }, JWT_SECRET, { expiresIn: '10h' });
+      res.json({ message: 'Logged in successfully', token });
+   } catch (error) {
+      res.status(500).json({ error: 'Failed to log in' });
+   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
-import express, {Request,Response} from 'express';
-import {json} from 'body-parser';
+import express, { Request, Response } from 'express';
+import { json } from 'body-parser';
 import authRoutes from './routes/authRoute';
 
 const app = express();
 app.use(json());
 
-app.use('/auth', authRoutes)
+app.use('/auth', authRoutes);
 // app.get('/',
 //     (req: Request,res: Response)=>{
 //         console.log("test")
 //         res.send("Yes it works");
 // });
 
-const PORT = process.env.PORT || 6000; 
-app.listen(PORT,()=>{
+const PORT = process.env.PORT || 6000;
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-    })
+})
