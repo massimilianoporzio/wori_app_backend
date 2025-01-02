498b9aa2ea4a929ab4eba76c0ecb6bd079025e16
Massimiliano Porzio <massimiliano.porzio@gmail.com>
2025-01-02 21:13:04 +0100

    login da sistemare

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -29,5 +29,23 @@ export const login = async(req: Request, res: Response) => {
     //2. verify if email exists than check password
     //3. return message if pwd wrong
     //4. return TOKEN!
+    const {email, password} = req.body;
+    try {
     
+    const result = await pool.query(
+        'SELECT * FROM users WHERE email = $1 ',
+        [email]
+    );
+    const user = result.rows[0]; //prima riga di risultati (ne devo avere solo una)
+    res.status(201).json({message: 'User registered successfully',user});
+    if(!user) return res.status(404).json({error: 'User not found'});
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if(!isMatch) return res.status(400).json({error: 'Invalid credentials'});
+
+    const token = jwt.sign({id: user.id}, JWT_SECRET, {expiresIn: '10h'});
+    res.json()
+ } catch (error) {
+    res.status(500).json({error: 'Failed to register user'});
+ }
 }
--- src/routes/authRoute.ts
+++ src/routes/authRoute.ts
@@ -4,6 +4,6 @@ import { login, register } from '../controllers/authController';
 const router = Router();
 
 router.post('/register',register);
-router.post('/login',login);
+// router.post('/login',login);
 
 export default router;
